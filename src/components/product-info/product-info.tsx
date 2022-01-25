36f2ac192555774db7bc374a2528b43fa2c9ce0d
Ivan <work.gorobec@gmail.com>
2022-01-26 02:38:12 +0500

    добавит страницу product-info

---
+++ src/components/product-info/product-info.tsx
@@ -0,0 +1,32 @@
+import {Guitar} from '../../types/guitar';
+import Rating from '../rating/rating';
+import Tabs from '../tabs/tabs';
+
+type ProductInfoProps = {
+  productInfo: Guitar;
+}
+
+function ProductInfo({productInfo}:ProductInfoProps):JSX.Element {
+  const {name, price, previewImg, rating} = productInfo;
+
+  return (
+    <div className="product-container">
+      <img className="product-container__img" src={previewImg} width="90" height="235" alt={name}/>
+      <div className="product-container__info-wrapper">
+        <h2 className="product-container__title title title--big title--uppercase">{name}</h2>
+        <div className="rate product-container__rating" aria-hidden="true">
+          <Rating guitarRating={rating}/>
+          <span className="rate__count"></span><span className="rate__message"></span>
+        </div>
+        <Tabs productInfo={productInfo} />
+      </div>
+      <div className="product-container__price-wrapper">
+        <p className="product-container__price-info product-container__price-info--title">Цена:</p>
+        <p className="product-container__price-info product-container__price-info--value">{price} ₽</p>
+        <a className="button button--red button--big product-container__button" href="#">Добавить в корзину</a>
+      </div>
+    </div>
+  );
+}
+
+export default ProductInfo;
