3bae3c2a2471e179b9501fca082d0822e7b19b29
Ivan <work.gorobec@gmail.com>
2022-02-08 02:03:26 +0500

    добавит логику сохранения и подзагрузки state из localStorage

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,6 +1,27 @@
 import {combineReducers, configureStore} from '@reduxjs/toolkit';
 import {mainAPI} from '../service/api';
 import {cartData} from './cart-reducer/cart-reducer';
+import {toast} from 'react-toastify';
+
+const loadCartFromLocalStorage = () => {
+  try {
+    const storageState = window.localStorage.getItem('state');
+    return storageState ? JSON.parse(storageState) : undefined;
+  } catch (error) {
+    toast.error(`${error} возникла ошибка`);
+    return undefined;
+  }
+};
+
+export const saveCartToLocalStorage = (state: State) => {
+  try {
+    window.localStorage.setItem('state', JSON.stringify(state));
+  } catch (error) {
+    toast.error(`${error} возникла ошибка`);
+  }
+};
+
+const preloadedState = loadCartFromLocalStorage();
 
 const rootReducer = combineReducers({
   [mainAPI.reducerPath]: mainAPI.reducer,
@@ -9,6 +30,7 @@ const rootReducer = combineReducers({
 
 export const setUpStore = () => configureStore( {
   reducer: rootReducer,
+  preloadedState: preloadedState,
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(mainAPI.middleware),
 });
