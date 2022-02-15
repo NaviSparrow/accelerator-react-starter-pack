db33e602998bedd474b843155199da57819066f3
Maxim Volkov <mx_ruz_net@hotmail.com>
2022-02-15 11:26:52 +0300

    another guard agains non-array

--- src/components/guitar-cards-list/guitar-cards-list.tsx
+++ src/components/guitar-cards-list/guitar-cards-list.tsx
@@ -61,7 +61,7 @@ function GuitarCardsList({guitarsList, viewState, onChangeURL}:GuitarCardsListPr
   return (
     <>
       <div className="cards catalog__cards">
-        {guitarsList && guitarsList.response.map((guitar) => (<GuitarCard key={guitar.id} guitarInfo={guitar} />))}
+        {guitarsList && Array.isArray(guitarsList.response) && guitarsList.response.map((guitar) => (<GuitarCard key={guitar.id} guitarInfo={guitar} />))}
       </div>
       <div className="pagination page-content__pagination">
         <ul className="pagination__list">
